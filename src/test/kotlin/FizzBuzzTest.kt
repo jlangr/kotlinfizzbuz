43a41aca81354b8f66ee6ed23b2a0338d68a87b9
Paul Moore <pdmoore@gmail.com>
2020-09-29 20:42:30 -0700

    initial failing test

---
+++ src/test/kotlin/FizzBuzzTest.kt
@@ -0,0 +1,13 @@
+package test.kotlin
+
+import org.junit.jupiter.api.Test
+import kotlin.test.fail
+
+class FizzBuzzTest {
+
+    @Test
+    fun `returns 1 for 1`() {
+        fail("oops - broken")
+    }
+
+}
